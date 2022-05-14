4310f484dd8e5e9b84f87c2173d26cc6ad26ac6f
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-05-14 17:03:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -76,10 +76,9 @@ m1 = folium.Map(location=[59.93, 30.33],
                 zoom_start=13)
 st_data = st_folium(m1, width = 725)
 st_data
-m2 = folium.Map(
-    location=[59.93, 30.33],
-    tiles='openstreetmap',
-    zoom_start=14)
-st1_data = st_folium(m2, width = 725)
-st1_data
+folium.Marker(
+    location=[59.94, 30.35],
+    popup='<i>Здесь был Вася</i>',
+    tooltip='Метка 1').add_to(m2) # попробуйте добавить: icon=folium.Icon(icon="cloud")
+
 
