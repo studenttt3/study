8f47fea741bed48b7eb0900f80133d21ba7f0c27
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-05-14 19:35:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -94,11 +94,10 @@ st.markdown("now let's take the height, weight and body mass index data of 500 r
 fig, ax = plt.subplots(figsize=(30,40), dpi= 80)
 sns.stripplot(df_2.Height, df_2.Weight, jitter=0.25, size=8, ax=ax, linewidth=.7, orient='h')
 plt.title('Height and weight of people from a sample size of 500 people', fontsize=30)
-plt.title('Dependence of GDP on Investment', fontsize=40)
 plt.xticks(fontsize=5)
 plt.yticks(fontsize=30)
-ax.set_xlabel("GDP2015", fontsize=30)
-ax.set_ylabel("Investments2015", fontsize=30)
+ax.set_xlabel("Height", fontsize=30)
+ax.set_ylabel("Weight", fontsize=30)
 st.pyplot(fig)
 
 fig = plt.figure(figsize=(15, 12), dpi= 80)
@@ -133,6 +132,7 @@ fig = plt.figure(
     colors = colors,
     figsize = (20, 15)
 )
+plt.title('Distribution of body mass index', fontsize=30)
 st.pyplot(fig)
 
 
