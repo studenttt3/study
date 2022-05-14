80443c9c11b5f2b54f07efaf39e2a0d56cf45c31
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-05-14 15:37:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -69,5 +69,5 @@ gen = st.selectbox(
 if(the_most == 'the highest and gen == men):
     fig, ax = plt.subplots(figsize=(16,10), dpi= 80)
     ax.vlines(x=df_1['Country Name'], ymin =0, color='blue', alpha=0.7, linewidth=2)
-    ax.scatter(x=df_1['Country Name'], y=df_1.['Male Height in Cm'], s=75, color='blue', alpha=0.7)
+    ax.scatter(x=df_1['Country Name'], y=df_1['Male Height in Cm'], s=75, color='blue', alpha=0.7)
     st.pyplot(fig)
