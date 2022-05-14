be429b9fc2a2ed288bb33fbc7d621efe62222a4e
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-05-14 11:40:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ fig, ax = plt.subplots(figsize=(30,10), dpi= 80)
 sns.stripplot(df_2.Height, df_2.Weight, jitter=0.25, size=8, ax=ax, linewidth=.5)
 st.pyplot(fig)
 
+df_counts = df_2.groupby(['Index']).size().reset_index(name='counts')
+
 fig, ax = plt.subplots(figsize=(16,10), dpi= 80)
 ax.vlines(x=df_counts.Index, ymin=0, ymax=df_counts.counts, color='blue', alpha=0.7, linewidth=2)
 ax.scatter(x=df_counts.Index, y=df_counts.counts, s=75, color='blue', alpha=0.7)
