b2b3c19dc07eff6f07bccc8c578d658f4b8e114f
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-05-14 10:41:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import numpy as np
 import seaborn as sns
 import streamlit as st
 
-df = pd.read_csv("Height.csv")
-df
+df_1 = pd.read_csv("Height.csv")
+df_1
+df_2 = pd.read_csv("500.csv")
+df_2
+df_3 = pd.read_csv("mass.csv")
+df_3
