d8e86f56e906730c9749ab1099144178c3858634
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-05-14 19:02:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,45 +14,6 @@ df_1 = pd.read_csv("Height.csv")
 df_2 = pd.read_csv("500.csv")
 df_3 = pd.read_csv("mass.csv")
 
-fig, ax = plt.subplots(figsize=(30,40), dpi= 80)
-sns.stripplot(df_2.Height, df_2.Weight, jitter=0.25, size=8, ax=ax, linewidth=.7, orient='h')
-plt.title('Height and weight of people from a sample size of 500 people', fontsize=30)
-st.pyplot(fig)
-
-fig = plt.figure(figsize=(16,8), dpi= 80)
-sns.boxplot(x='Gender', y='Height', data=df_2, notch=False)
-plt.title('Height', fontsize=30)
-st.pyplot(fig)
-
-fig = plt.figure(figsize=(15,12), dpi= 80)
-sns.boxplot(x='Gender', y='Weight', data=df_2, notch=False)
-plt.title('Weight', fontsize=30)
-st.pyplot(fig)
-
-fig = plt.figure(figsize=(15,10), dpi= 80)
-sns.boxplot(x='Gender', y='Index', data=df_2, notch=False)
-plt.title('Mass index', fontsize=30)
-st.pyplot(fig)
-
-df_counts = df_2.groupby(['Index']).size().reset_index(name='counts')
-
-categories = df_counts.shape[0]
-colors = [plt.cm.inferno_r(i/float(categories)) for i in range(categories)]
-fig = plt.figure(
-    FigureClass = Waffle,
-    plots = {
-        111: {
-            'values': df_counts['counts'],
-            'labels': ["{0}".format(n[0], n[1]) for n in df_counts[['Index', 'counts']].itertuples()],
-            'legend': {'loc': 'upper left', 'bbox_to_anchor': (1.1, 1.2), 'fontsize': 22},
-            'title': {'label': 'Distribution of body mass index', 'loc': 'center', 'fontsize':30}
-        },
-    },
-    rows = 10,
-    colors = colors,
-    figsize = (20, 15)
-)
-st.pyplot(fig)
 
 country = st.selectbox(
         "Country Name", df_1["Country Name"].value_counts().index)
@@ -97,13 +58,52 @@ st.header("The country with the largest growth of men on the map")
 loc = 'Netherlands'
 location = geocode(loc, provider="nominatim" , user_agent = 'my_request')
 point = location.geometry.iloc[0] 
-
 data= pd.DataFrame({"longitude":[point.x], "latitude":[point.y]})
-
 mapit = folium.Map( location=[0, 0], zoom_start=1 ) 
 for lat , lon in zip(data.latitude , data.longitude): 
         folium.Marker( location=[ lat,lon ], fill_color='#43d9de', radius=8 ).add_to( mapit ) 
 st_data = st_folium(mapit, width = 725)
 st_data
+
+fig, ax = plt.subplots(figsize=(30,40), dpi= 80)
+sns.stripplot(df_2.Height, df_2.Weight, jitter=0.25, size=8, ax=ax, linewidth=.7, orient='h')
+plt.title('Height and weight of people from a sample size of 500 people', fontsize=30)
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(15, 12), dpi= 80)
+sns.boxplot(x='Gender', y='Height', data=df_2, notch=False)
+plt.title('Height', fontsize=30)
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(15, 12), dpi= 80)
+sns.boxplot(x='Gender', y='Weight', data=df_2, notch=False)
+plt.title('Weight', fontsize=30)
+st.pyplot(fig)
+
+fig = plt.figure(figsize=(15, 12), dpi= 80)
+sns.boxplot(x='Gender', y='Index', data=df_2, notch=False)
+plt.title('Mass index', fontsize=30)
+st.pyplot(fig)
+
+df_counts = df_2.groupby(['Index']).size().reset_index(name='counts')
+
+categories = df_counts.shape[0]
+colors = [plt.cm.inferno_r(i/float(categories)) for i in range(categories)]
+fig = plt.figure(
+    FigureClass = Waffle,
+    plots = {
+        111: {
+            'values': df_counts['counts'],
+            'labels': ["{0}".format(n[0], n[1]) for n in df_counts[['Index', 'counts']].itertuples()],
+            'legend': {'loc': 'upper left', 'bbox_to_anchor': (1.1, 1.2), 'fontsize': 22},
+            'title': {'label': 'Distribution of body mass index', 'loc': 'center', 'fontsize':30}
+        },
+    },
+    rows = 10,
+    colors = colors,
+    figsize = (20, 15)
+)
+st.pyplot(fig)
+
 
 
