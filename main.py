d07db9233a5bd205c6c42212620211b428de63ff
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-05-14 10:46:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ df_2 = pd.read_csv("500.csv")
 df_2
 df_3 = pd.read_csv("mass.csv")
 df_3
+
+large = 22; med = 16; small = 12
+params = {'axes.titlesize': large,
+          'legend.fontsize': med,
+          'figure.figsize': (16, 10),
+          'axes.labelsize': med,
+          'axes.titlesize': med,
+          'xtick.labelsize': med,
+          'ytick.labelsize': med,
+          'figure.titlesize': large}
+plt.rcParams.update(params)
+plt.style.use('seaborn-whitegrid')
+sns.set_style("white")
+
+fig, ax = plt.subplots(figsize=(30,10), dpi= 80)
+sns.stripplot(df_2.Height, df_2.Weight, jitter=0.25, size=8, ax=ax, linewidth=.5)
