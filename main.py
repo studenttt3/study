71bf112bb0cf840dccf63885af4456f562d9e72b
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-05-14 19:25:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -77,16 +77,6 @@ for lat , lon in zip(data.latitude , data.longitude):
 st_data_1 = st_folium(mapit, width = 725)
 st_data_1
 
-st.header("The country with the largest growth of women on the map")  
-loc = 'Netherlands'
-location = geocode(loc, provider="nominatim" , user_agent = 'my_request')
-point = location.geometry.iloc[0] 
-data= pd.DataFrame({"longitude":[point.x], "latitude":[point.y]})
-mapit = folium.Map( location=[0, 0], zoom_start=1 ) 
-for lat , lon in zip(data.latitude , data.longitude): 
-        folium.Marker( location=[ lat,lon ], fill_color='#43d9de', radius=8 ).add_to( mapit ) 
-st_data_2 = st_folium(mapit, width = 725)
-st_data_2
 
 st.header("The country with the lowest growth of women on the map")    
 loc = 'Guatemala'
